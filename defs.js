c170e5590957cee8a66202f6f96346403806cd41
olivialy524 <olivialy524@gmail.com>
2019-11-23 23:41:32 -0500

    bouncing effect when drop

--- defs.js
+++ defs.js
@@ -9,31 +9,45 @@ var thetaShift = 0;
 var phiShift = 0;
 var curTheta = 0;
 var curPhi = 0;
+var moveShape = false;
+var initShapePos = [0, 0];
+
+var mouse = {
+    mouseDown: false,
+    mouseDragged: false,
+}
+var prevMouseDown = false;
+
+// drag with momentum
+var friction = 0.80;
+var stopThreshold = 0.0001;
+var trackingPoints = [];
+var decelerating = false;
 
 // axis
 var xAxis = vec3.fromValues(1, 0, 0);
 var yAxis = vec3.fromValues(0, 1, 0);
 var zAxis = vec3.fromValues(0, 0, 1);
 
-
 //textrue
 var textures = [];
-
-var moveShape = false;
-var initShapePos = [0, 0];
 
 var disp_x = 0.0;
 var disp_y = 0.0;
 
-var friction = 0.80;
-var stopThreshold = 0.0001;
+// draw cube positions
+var dir = 0;
+var lastX = 5;
+var lastY = 5;
+var dis = 0;
 
-var trackingPoints = [];
-var decelerating = false;
+// draw cubes and ground
+var all_cubes = [];
+var new_cube = [];
+var ground = [];
+var new_ground = [];
 
-var mouse = {
-    mouseDown: false,
-    mouseDragged: false,
-}
-
-var prevMouseDown = false;
+// transformations
+var transform_matrix_1 = mat4.create();
+var transform_matrix_2 = mat4.create();
+var transform_matrix_3 = mat4.create();
