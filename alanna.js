57baadd18a1cf53b568c41cbe99ab02f433a85fd
asz33@cornell.edu <asz33@cornell.edu>
2019-12-10 13:18:43 -0500

    Now only character is using vertex1 frag1

--- alanna.js
+++ alanna.js
@@ -1,33 +1,43 @@
-function drawRainDrop(transmat, dropmat) {
-  rain.forEach(cubes => (cubes.shapes).forEach(shape => drawShape(gl, shape, cubes.texture, transmat, false)));
-  (new_rainDrop.shapes).forEach(shape => drawShape(gl, shape, new_rainDrop.texture, dropmat, false));
-}
+function drawShadow(gl, shape, texture, transmat, isCharacter, scalemat) {
+  program1.draw(gl, shape, function () {
+    if (program1.xform_projMat != null) {
+      gl.uniformMatrix4fv(program1.xform_projMat, false, getProjMatrix());
+    }
+    if (program1.xform_viewMat != null) {
+      gl.uniformMatrix4fv(program1.xform_viewMat, false, getViewMatrix());
+    }
+    if (program1.xform_modelMat != null) {
+      gl.uniformMatrix4fv(program1.xform_modelMat, false, getModelMatrix());
+    }
+    if (transmat != null) {
+      if (program1.transform_Mat != null) {
+        gl.uniformMatrix4fv(program1.transform_Mat, false, transmat);
+      }
+    }
+    if (scalemat != null) {
+      if (program1.xform_scaleMat != null) {
+        gl.uniformMatrix4fv(program1.xform_scaleMat, false, scalemat);
+      }
+    }
+    if (program1.xform_normalMat != null) {
+      gl.uniformMatrix4fv(program1.xform_normalMat, false, getNormalMatrix());
+    }
+    if (program1.texture != null) {
+      gl.activeTexture(gl.TEXTURE0);
+      gl.bindTexture(gl.TEXTURE_2D, texture);
+      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+      gl.uniform1i(program1.texture, 0);
+    }
+    if (program1.isCharacter != null) {
+      if (isCharacter) {
+        gl.uniform1f(program1.isCharacter, 1.0);
+      } else {
+        gl.uniform1f(program1.isCharacter, 0.0);
+      }
+    }
+  });
 
-function dropRain(transmat, dropmat) {
-  drawRainDrop(transmat, dropmat);
-  if (rainHeight > -5) {
-    rainHeight = rainHeight - 0.01;
-    if (rainHeight <= -5)
-      bouncing = true;
-    var drop = vec3.create();
-    vec3.set(drop, 0, 0, rainHeight);
-    var droptranslation = mat4.fromTranslation(mat4.clone(dropmat), drop);
-    transform_rain_2 = droptranslation;
-    window.requestAnimationFrame(() => dropRain(transmat, droptranslation));
-  }
-}
-
-function resetRainDropMatrices() {
-  init = false;
-  rain.push(new_rainDrop);
-  if (rain.length >= 7) {
-    rain.shift();
-    ground.shift();
-
-  }
-  rainHeight = 0;
-  maxHeight = 1;
-  bouncing = false;
-  transform_rain_1 = mat4.clone(transform_rain_2);
-  transform_rain_2 = mat4.create();
 }
