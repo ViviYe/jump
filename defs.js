fd7124a40ab8b4d7e52ecb1aeb6c0221817ed0f5
asz33@cornell.edu <asz33@cornell.edu>
2019-12-10 22:24:37 -0500

    Refactor

--- defs.js
+++ defs.js
@@ -1,4 +1,4 @@
-//click and drag
+// click and drag
 var program0; var projDist = 50;
 var oldFov = 30;
 var isScroll = false;
@@ -16,7 +16,7 @@ var yAxis = vec3.fromValues(0, 1, 0);
 var zAxis = vec3.fromValues(0, 0, 1);
 
 
-//textrue
+// texture
 var textures = [];
 var cubetextures = [];
 
@@ -34,25 +34,21 @@ var trackingPoints = [];
 var decelerating = false;
 
 var mouse = {
-    mouseDown: false,
-    mouseDragged: false,
+  mouseDown: false,
+  mouseDragged: false,
 }
 var prevMouseDown = false;
 
-//init height when drop a cube
+// init height when drop a cube
 var height = 0;
 
-//matrices init
+// matrices init
 var transform_matrix_1 = mat4.create();
 var transform_matrix_2 = mat4.create();
 var transform_matrix_3 = mat4.create();
 
 var transform_rain_1 = mat4.create();
 var transform_rain_2 = mat4.create();
-
-// var compress_time;
-// var release_time;
-// var prevShiftDown = false;
 
 var characterheight = 0;
 var all_cubes = [];
@@ -78,20 +74,5 @@ var clientRect = webglCanvas[0].getBoundingClientRect();
 var NO_CONTROL = 0;
 var IS_ROTATING = 1;
 var IS_MOVING = 2;
-
-// var control = {
-//     mode: NO_CONTROL,
-//     rotateRate: 0.005, //0.004,
-//     moveRate: 0.005,
-//     displacement: null,
-//     currentHeading: 0.6,
-//     fromHeading: null,
-//     toHeading: null,
-//     currentPosition: vec3.fromValues(0 * 1.0 + 0.5,
-//         1 * 1.0 + 0.5,
-//         0.0),
-//     fromPosition: [0, 1],
-//     toPosition: null
-// };
 
 var init = true;
